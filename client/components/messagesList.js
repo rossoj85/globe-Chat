6fbef06744200dd636c2d96d1493a433e257beae
Jason Rosso <jasonrosso@gmail.com>
2018-11-05 14:29:22 -0500

    before channel overhaul

--- client/components/messagesList.js
+++ client/components/messagesList.js
@@ -12,10 +12,10 @@ class MessagesList extends Component {
 
 
   render () {
-  
+  console.log('NEW MESSAGE LIST MOUNTED')
     // const channelId = Number(this.props.match.params.channelId); // because it's a string "1", not a number!
     // const filteredMessages = messages.filter(message => message.channelId === channelId);
-    // console.log("!#$#@MESSAGE LIST PROPS$#@!$!@",this.props)
+    console.log("!#$#@MESSAGE LIST PROPS$#@!$!@",this.props)
     const messages = this.props.messagesCollection
     const channelId= this.props.channelId
     const userId = this.props.currentUser && this.props.currentUser.id
